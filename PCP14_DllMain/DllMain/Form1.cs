c56c5f6a0c5f545b39fb1c1e0357db0ac359875d
d2h10s <ecanus9@gmail.com>
2020-06-19 14:27:41 +0900

    Labeling

---
+++ PCP14_DllMain/DllMain/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DllMain
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnInCSharp_Click(object sender, EventArgs e)
+        {
+            lblNPrime.Text = "...";
+            lblTime.Text = "...";
+            Application.DoEvents();
+
+            int nMax = Convert.ToInt32(txtNMax.Text);
+            int nprime;
+
+            DateTime stime = DateTime.Now;
+            PrimeCSharp.FindNumberOfPrimeNumber(nMax, out nprime);
+            double dtime = Util.TimeInSeconds(stime);
+
+            lblNPrime.Text = Convert.ToString(nprime);
+            lblTime.Text = string.Format("{0:0.00}", dtime);
+        }
+
+        private void btnInCPP_Click(object sender, EventArgs e)
+        {
+            lblNPrime.Text = "...";
+            lblTime.Text = "...";
+            Application.DoEvents();
+
+            int nMax = Convert.ToInt32(txtNMax.Text);
+            int nprime = 0;
+
+            DateTime stime = DateTime.Now;
+            dllPrime.dllFindNumberOfPrimeNumber(nMax, ref nprime);
+            double dtime = Util.TimeInSeconds(stime);
+
+            lblNPrime.Text = Convert.ToString(nprime);
+            lblTime.Text = string.Format("{0:0.00}", dtime);
+        }
+    }
+}
---
+++ PCP14_DllMain/DllMain/dllPrime.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace DllMain
+{
+    static class dllPrime
+    {
+        [DllImport("PrimeCPP.dll",
+                             CallingConvention = CallingConvention.Cdecl)]
+        public static extern void dllFindNumberOfPrimeNumber(int nMax, ref int nprime);
+    }
+}
